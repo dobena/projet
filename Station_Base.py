8db0b838f6d4d510f756d0a9b045e96e92ef9571
dobena <61889068+dobena@users.noreply.github.com>
2020-03-22 17:05:05 -0400

    test2 print station de base

--- Station_Base.py
+++ Station_Base.py
@@ -1,1 +1,35 @@
+
+from threading import Thread
+
+
 print("station de base!")
+
+
+
+
+class StationBase(Thread):
+    def __init__(self, id, position, tampon, puissanceMax, nbUEConnectés):
+        Thread.__init__(self)
+        self.id = id  # integer
+        self.position = position # array
+        self.tampon = tampon # Queue
+        self.puissanceMax = puissanceMax# Double
+        self.nbUEConnectés = nbUEConnectés   # integer
+        print("La station de base est crée")
+    # # methodes or functions
+    def Reception_Message_1(self):
+      print("Reception_Message_1")
+
+    def Envoi_Message_2(self):
+      print("Envoi_Message_2")
+
+    def Reception_Message_3(self):
+      print("Reception_Message_3")
+
+    def Envoi_Message_4(self):
+      print("Envoi_Message_4")
+
+    def ecouterCanal(self):
+      print("ecouterCanal")
+
+
